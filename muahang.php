0558039f09263e2837d8962d5af32ca95bc03108
ngothimylanh <mylanh19042002@gmail.com>
2022-12-29 13:51:46 +0700

    them file muahang

---
+++ muahang.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>SHOP</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+</head>
+<body>
+<div class="hoso">
+<h2> &emsp;&ensp;Đặt hàng thành công </h2>
+<br><br>
+<a href='index.php' title='index'>
+    <input type= "button" name = "index" value = "Tiếp tục mua hàng">
+</a>
+</div>
+</body>
