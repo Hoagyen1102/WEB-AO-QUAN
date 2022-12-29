9394b24a8798cbbaf34f97c52a5697143515e1eb
ngothimylanh <mylanh19042002@gmail.com>
2022-12-29 13:21:52 +0700

    them file xulygiohang

---
+++ xulygiohang.php
@@ -0,0 +1,22 @@
+<?php
+include('ketnoi.php');
+$prd_id=$_GET['id'];
+if (isset($_SESSION['username']) && $_SESSION['username']) {
+    $username = $_SESSION['username'];
+}else{
+    $username = "khách";
+}
+$row = mysqli_fetch_assoc(mysqli_query($conn,"SELECT * from cart where prd_id=$prd_id"));
+if((mysqli_num_rows(mysqli_query($conn,"SELECT * from cart where prd_id=$prd_id")) >  0) && ($row['username']==$username)) {
+    $quantity = $row['quantity'] + 1;
+    $sql_up = "UPDATE cart SET quantity = $quantity where prd_id = $prd_id";
+}
+else{
+    $quantity = 1;
+    $sql_up = "INSERT into cart (username, prd_id, quantity)
+    values ('$username', $prd_id, $quantity)";
+}
+
+$query_up = mysqli_query($conn, $sql_up);
+header('location: index.php?quanly=giohang')
+?>
